2901f89ff47db2ddf90efeec218107ee593ef5c9
Ruth Baker <ruthmoog@gmail.com>
2019-05-07 18:47:43 +0100

    Remove uneccessary comments

--- text_adventure_game.rb
+++ text_adventure_game.rb
@@ -1,22 +1,3 @@
-
-# Tell user they're facing forward & game starts
-# Give user their options
-# Get user's input
-# If the move right tell them they died via goblin & game ends
-# If they move left tell them they died via werewolf & game ends
-# If they move forward, repeat
-# If they move forward twice tell them they win and game ends
-
-# The user is told they're facing forward and can type 'forward', 'left', or 'right'.
-# If the user enters 'right', they die (goblin).
-# If the user enters 'left', they die (werewolf).
-# If the user enters 'forward', they're told they live, and can move again.
-# If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 1 again.
-# If the user enters 'right', they die (goblin).
-# If the user enters 'left', they die (werewolf).
-# If the user enters 'forward', they're told they won and the game exits.
-# If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 4 again.
-
 turns = 2
 
 puts "You are facing forward, and the game begins."
