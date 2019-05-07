e0bbc223c4aaffcc6a2339b88ca8debf652ef42f
Ruth Baker <ruthmoog@gmail.com>
2019-05-07 18:41:27 +0100

    Allow user to keep playing until they take all their turns

--- text_adventure_game.rb
+++ text_adventure_game.rb
@@ -17,16 +17,23 @@
 # If the user enters 'forward', they're told they won and the game exits.
 # If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 4 again.
 
+turns = 2
+
 puts "You are facing forward, and the game begins."
-puts "To move right, type R.  To move left, type L.  To move forward, type F."
-puts "Where would you like to move? ..."
-move = gets.chomp.upcase
-if move[0] == "R"
-  abort("You were turned to stone by a goblin.  Sorry, you're dead!")
-elsif move[0] == "L"
-  abort("You were disembowled by a werewolf. Sorry, you're dead!")
-elsif move[0] == "F"
-  puts "You avoided danger and may move again"
-else
-  puts "OK"
+while turns != 0
+  puts "To move right, type R.  To move left, type L.  To move forward, type F."
+  puts "Where would you like to move? ..."
+  move = gets.chomp.upcase
+  if move[0] == "R"
+    abort("You were turned to stone by a goblin.  Sorry, you're dead!")
+  elsif move[0] == "L"
+    abort("You were disembowled by a werewolf. Sorry, you're dead!")
+  elsif move[0] == "F"
+    puts "You avoided danger and may move again"
+    turns -= 1
+  else
+    puts OK
+  end
 end
+puts "Congratulations you won!"
+puts "-The End-"
